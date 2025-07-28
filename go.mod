a72d3a256aa19821f12f5d8e67dda34bfd327aaa
Shaswat Shah <99328539+sdshah09@users.noreply.github.com>
2025-07-28 17:19:09 -0500

    add repository and service layer for account microservice

--- go.mod
+++ go.mod
@@ -5,6 +5,8 @@ go 1.24.5
 require (
 	github.com/99designs/gqlgen v0.17.78
 	github.com/kelseyhightower/envconfig v1.4.0
+	github.com/lib/pq v1.10.9
+	github.com/segmentio/ksuid v1.0.4
 	github.com/vektah/gqlparser/v2 v2.5.30
 )
 
--- graphql/graph.go
+++ graphql/graph.go
@@ -5,37 +5,37 @@ package main
 
 
 type Server struct {
-// 	accountClient *account.Client
-// 	productClient *product.Client
-// 	orderClient *order.Client
+	// accountClient *account.Client
+	// productClient *product.Client
+	// orderClient *order.Client
 }
 
 // *Server pointer means we return reference because it is cheap rather than cerating instance and then returning it
-// func NewGraphQLServer(accountUrl, productUrl, orderUrl string)  (*Server, error) {
-// 	accountClient, err := account.NewClient(accountUrl)
-// 	if err != nil {
-// 		return nil, err
-// 	}
+func NewGraphQLServer(accountUrl, productUrl, orderUrl string)  (*Server, error) {
+	// accountClient, err := account.NewClient(accountUrl)
+	// if err != nil {
+	// 	return nil, err
+	// }
 
-// 	productClient, err := product.NewClient(productUrl)
-// 	if err != nil {
-// 		accountClient.close()
-// 		return nil, err
-// 	}
+	// productClient, err := product.NewClient(productUrl)
+	// if err != nil {
+	// 	accountClient.close()
+	// 	return nil, err
+	// }
 
-// 	orderClient, err := order.NewClient(orderUrl)
-// 	if err != nil {
-// 		accountClient.close()
-// 		productClient.close()
-// 		return nil, err
-// 	}
+	// orderClient, err := order.NewClient(orderUrl)
+	// if err != nil {
+	// 	accountClient.close()
+	// 	productClient.close()
+	// 	return nil, err
+	// }
 
-// 	return &Server{
-// 		accountClient,
-// 		productClient,
-// 		orderClient,
-// 	}, nil
-// }
+	return &Server{
+		// accountClient,
+		// productClient,
+		// orderClient,
+	}, nil
+}
 
 // func (s *Server) Mutation() MutationResolver {
 // 	return  &mutationResolver{
@@ -55,8 +55,8 @@ type Server struct {
 // 	}
 // }
 
-// func (s *Server) ToExecutableSchema() graphql.ExecutableSchema {
-// 	return NewExecutableSchema(Config{
-// 		Resolvers: s,
-// 	})
-// }
+func (s *Server) ToExecutableSchema() graphql.ExecutableSchema {
+	return NewExecutableSchema(Config{
+		Resolvers: s,
+	})
+}
